4d516968b5d874e7be972d09b527b31c6ced8d0f
Prakhar Mishra <prakhargreen15@gmail.com>
2022-07-17 06:38:21 +0530

    none

--- Reverse of an Array.cpp
+++ Reverse of an Array.cpp
@@ -4,5 +4,13 @@ int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
+    vector<int> arr;
+    for(int i=0 ; i<6; i++)
+    {
+        arr.push_back(i);
+        cout<<arr[i];
+    }
+    for (auto i = arr.begin(); i != arr.end(); ++i)
+        cout << *i << " ";
     return 0;
 }
